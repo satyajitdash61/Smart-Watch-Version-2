7f27d27b96ddf326162a738fb062b4e03cd4a7dd
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-10-10 11:00:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// creating object of Date
-
 let currentdate=new Date();
 const day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const month=['January','February','March','April','May','June','July','August','September','October','November','December'];
@@ -8,128 +6,119 @@ let seconds=0;
 let minutes=0;
 let hours=0;
 
+
 let displaySeconds=0;
 let displayMinutes=0;
 let displayHours=0;
 
 let interval = null;
 let lap = "00:00:00"; 
-
-// When the screen opens
+let status=0;
+function tm(){
+    $("#welcomescreen").show();
+    if(currentdate.getHours()<10){
+        $("#welcomeHr").text("0"+currentdate.getHours());
+    }
+    else{
+        $("#welcomeHr").text(currentdate.getHours());
+    }
+    if(currentdate.getMinutes()<10){
+        $("#welcomeMin").text("0"+currentdate.getMinutes());
+    }
+    else{
+        $("#welcomeMin").text(currentdate.getMinutes());
+    }
+    $("#welcomeDt").text(day[currentdate.getDay()]+", "+month[currentdate.getMonth()]+" "+currentdate.getDate());
+}
 
 $(document).ready(function(){
     $("#showTime").hide();
-    //function tm(){
-    if(currentdate.getHours()<10){
-        $("#hr").text("0"+currentdate.getHours());
-    }
-    else{
-        $("#hr").text(currentdate.getHours());
-    }
-    if(currentdate.getMinutes()<10){
-        $("#min").text("0"+currentdate.getMinutes());
-    }
-    else{
-        $("#min").text(currentdate.getMinutes());
-    }
-    $("#dt").text(day[currentdate.getDay()]+", "+month[currentdate.getMonth()]+" "+currentdate.getDate());
-//}
-//window.setInterval(tm,1000);
     $("#messageFunc,#showMessage,#musicPlayer,#clock").hide();
+    tm();
+//window.setInterval(tm,1000);
+    
 })
-
-// On clicking the message buttton
-
 $("#messageButton").click(function(){
     $("#showTime").text(currentdate.getHours()+":"+currentdate.getMinutes());
     $("#showTime,#messageFunc").show();
     // $("#messageFunc").show();
     $("#messageButton").css("color","rgb(24, 240, 89)");
     $("#musicButton,#timerButton").css("color","rgb(34, 36, 34)");
-    $("#hr,#min,#dt,#showMessage,#musicPlayer,#clock").hide();
+    $("#home").css("color","rgb(156, 156, 156)");
+    $("#hr,#min,#dt,#showMessage,#musicPlayer,#clock,#welcomescreen").hide();
+    status=1;
 })
-
-// On clicking the Music buttton
-
 $("#musicButton").click(function(){
     $("#showTime").text(currentdate.getHours()+":"+currentdate.getMinutes());
     $("#showTime,#musicPlayer").show();
     $("#messageButton,#timerButton").css("color","rgb(34, 36, 34)");
     $("#musicButton").css("color","rgb(24, 240, 89)");
-    $("#hr,#min,#dt,#messageFunc,#showMessage,#clock").hide();
+    $("#home").css("color","rgb(156, 156, 156)");
+    $("#hr,#min,#dt,#messageFunc,#showMessage,#clock,#welcomescreen").hide();
+    status=1;
 })
-
-// On clicking the Timer buttton
-
-
 $("#timerButton").click(function(){
     $("#showTime").text(currentdate.getHours()+":"+currentdate.getMinutes());
     $("#showTime,#clock,#play").show();
     $("#messageButton,#musicButton").css("color","rgb(34, 36, 34)");
     $("#timerButton").css("color","rgb(24, 240, 89)");
-    $("#hr,#min,#dt,#messageFunc,#showMessage,#musicPlayer,#pause").hide();
+    $("#home").css("color","rgb(156, 156, 156)");
+    $("#hr,#min,#dt,#messageFunc,#showMessage,#musicPlayer,#pause,#welcomescreen").hide();
+    
+    if(status==1){
+        reset();
+        status=0;
+    }
+    reset()
+    
 })
-
-// On clicking the first message buttton
-
 
 $("#firstMsg").click(function(){
     $("#showMessage,#firstMsgShow").show();
     //$("#firstMsgShow").show();
-    $("#messageFunc,#secondMsgShow,#thirdMsgShow").hide();
+    $("#messageFunc,#secondMsgShow,#thirdMsgShow,#welcomescreen").hide();
 })
-
-// On clicking the second message buttton
 
 $("#secondMsg").click(function(){
     $("#showMessage,#secondMsgShow").show();
     // $("#secondMsgShow").show();
-    $("#messageFunc,#firstMsgShow,#thirdMsgShow").hide();
+    $("#messageFunc,#firstMsgShow,#thirdMsgShow,#welcomescreen").hide();
 })
-
-// On clicking the third message buttton
 
 $("#thirdMsg").click(function(){
     $("#showMessage,#thirdMsgShow").show();
     // $("#thirdMsgShow").show();
-    $("#messageFunc,#secondMsgShow,#firstMsgShow").hide();
+    $("#messageFunc,#secondMsgShow,#firstMsgShow,#welcomescreen").hide();
 })
-
-// On clicking the play buttton in stopwatch
-
 $("#play").click(function(){
     $("#play").hide();
     $("#pause").show();
-
+if(status==0){
     interval=window.setInterval(sw, 1000);
+}
 })
-// On clicking the pause buttton in stopwatch
-
 $("#pause").click(function(){
     $("#pause").hide();
     $("#play").show();
     window.clearInterval(interval);
+    status=0;
 })
-// On clicking the reload buttton in stopwatch
-
-
 $("#reload").click(function(){
+    status=0;
     reset();
 })
-
-// On clicking the lap buttton in stopwatch
-
 $("#flag").click(function(){
     $("#showLap").text(lap);
 })
-// On clicking the home buttton 
-
 $("#home").click(function(){
     $("#home").css("color","rgb(24, 240, 89)");
-    location.reload(true);
+    $("#messageFunc,#showMessage,#musicPlayer,#clock,#showTime").hide();
+    tm();
+    $("#messageButton,#musicButton,#timerButton").css("color","rgb(34, 36, 34)");
+    status=1;
+    reset();
 })
 
-// Stop watch function
 
 function sw(){
     seconds++;
@@ -166,8 +155,6 @@ function sw(){
     $("#showStopWatch").text(displayHours+":"+displayMinutes+":"+displaySeconds);
     lap=displayHours+":"+displayMinutes+":"+displaySeconds;
 }
-
-//reset function of stop watch
 
 function reset(){
     window.clearInterval(interval);
